2b3e0ed5d63ef03c616f5b9f74012702404dc94b
DanThemes <danthemes@gmail.com>
2022-09-22 20:22:56 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import React, { useEffect, useState } from 'react';
 import fetchWord from './apis/dictionaryApi';
 import Content from './components/Content';
 import Header from './components/Header';
+import './app.css';
 
 const App = () => {
     const [word, setWord] = useState('star');
     const [wordData, setWordData] = useState({});
 
     useEffect(() => {
-        if (!word) return;
+        if (word) {
+            const fetch = async () => {
+                const data = await fetchWord(word);
 
-        const fetch = async () => {
-            const data = await fetchWord(word);
-
-            setWordData(data);
-        };
-        
-        fetch();
-    }, [])
+                setWordData(data);
+            };
+            
+            fetch();
+        }
+    }, [word])
 
     return (
+        wordData.length &&
         <div>
-            {/* {console.log(wordData)} */}
+            {console.log(wordData)}
             <Header setWord={setWord} />
             <Content wordData={wordData} />
         </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,59 @@
 import React from 'react'
 
 const Content = ({ wordData }) => {
-    console.log(wordData)
+    console.log(wordData);
+
+    const audio = (word) => {
+        return word.phonetics.map((phonetic, idx) => {
+            return phonetic.audio ? <audio controls key={idx} src={phonetic.audio}></audio> : '';
+        })
+    }
+
+    const meanings = (word) => {
+        return word.meanings.map((meaning, idx) => {
+            const partOfSpeech = <li key={idx} className="partOfSpeech">{meaning.partOfSpeech}</li>;
+
+
+            const definitions = meaning.definitions.map(({definition, example = ''}, idx) => {
+                const item = <li key={idx} value={idx+1} className="meaning">
+                    {definition} 
+                    {example && <span className="example">Exemple: {example}</span>}
+                </li>
+
+                return item;
+            });
+
+            return <ul>{[partOfSpeech, definitions]}</ul>;
+        })
+    }
+
     return ( 
-        wordData && (
-            <div>
-                Results for word: <span>{wordData[0].word}</span>
-                {/* <span>{wordData[0].phonetic}</span> */}
+        <div className="container">
+            <h4>Results:</h4>
+            <div className="content">
+                {wordData.map((word, idx) => {
+                    return (
+                        <div key={idx}>
+                            <div>
+                                <strong className="word">{word.word}</strong>
+                                <span className="phonetic">{word.phonetic}</span>
+                            </div>
+                            <div>
+                                <h4>Pronounciation:</h4>
+                                {audio(word)}
+                            </div>
+                            <div>
+                                <h4>Meanings:</h4>
+                                {console.log(meanings(word))}
+                                <ul>
+                                    {meanings(word)}
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
-        )
+        </div>
     )
 }
 
